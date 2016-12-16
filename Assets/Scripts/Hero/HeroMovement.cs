152c1752e565fd74e8ba0fb04df2e4722d4b9530
Amir Eldor <Amir Eldor>
2016-12-16 13:58:19 +0200

    Spritemovement with keyboard

--- Assets/Scripts/Hero/HeroMovement.cs
+++ Assets/Scripts/Hero/HeroMovement.cs
@@ -3,9 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HeroMovement : MonoBehaviour {
+
+	public float speed = 1f;
+
+	Rigidbody2D rigidbody;
+
+	void Awake() {
+		rigidbody = GetComponent<Rigidbody2D>();
+	}
+
 	void FixedUpdate() {
 		var h = Input.GetAxisRaw("Horizontal");
 		var v = Input.GetAxisRaw("Vertical");
-		transform.position = new Vector2(h, v);
+		var movement = new Vector2(h, v) * speed;
+		rigidbody.MovePosition(rigidbody.position + movement * Time.deltaTime);
 	}
 }
