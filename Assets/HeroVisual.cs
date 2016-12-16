e461545425960defe23670bbe2e469cd9a7a9bfe
Amir Eldor <Amir Eldor>
2016-12-16 21:18:11 +0200

    Split movement and flip-x to individual files

---
+++ Assets/HeroVisual.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroVisual : MonoBehaviour {
+	SpriteRenderer spriteRenderer;
+
+	void Awake() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	void Update() {
+		var h = Input.GetAxisRaw("Horizontal");
+		FlipXIfNeeded(h);
+	}
+
+	void FlipXIfNeeded(float h) {
+		bool isGoingRight = h > 0;
+		if (h != 0) {
+			spriteRenderer.flipX = isGoingRight;
+		}
+	}
+
+}
--- Assets/Scripts/Hero/HeroMovement.cs
+++ Assets/Scripts/Hero/HeroMovement.cs
@@ -7,24 +7,9 @@ public class HeroMovement : MonoBehaviour {
 	public float speed = 1f;
 
 	new Rigidbody2D rigidbody;
-	SpriteRenderer spriteRenderer;
 
 	void Awake() {
 		rigidbody = GetComponent<Rigidbody2D>();
-		spriteRenderer = GetComponent<SpriteRenderer>();
-	}
-
-	void Update() {
-		var h = Input.GetAxisRaw("Horizontal");
-		var v = Input.GetAxisRaw("Vertical");
-		FlipXIfNeeded(h);
-	}
-
-	void FlipXIfNeeded(float h) {
-		bool isGoingRight = h > 0;
-		if (h != 0) {
-			spriteRenderer.flipX = isGoingRight;
-		}
 	}
 
 	void FixedUpdate() {
