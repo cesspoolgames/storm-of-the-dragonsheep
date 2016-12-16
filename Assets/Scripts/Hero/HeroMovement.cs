5c3a3fcee578f71a591a4a5d9606cc9fc0f98d4c
Amir Eldor <Amir Eldor>
2016-12-16 13:15:28 +0200

    Initial commit. Hero stub and raw axis check

---
+++ Assets/Scripts/Hero/HeroMovement.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroMovement : MonoBehaviour {
+	void FixedUpdate() {
+		var h = Input.GetAxisRaw("Horizontal");
+		var v = Input.GetAxisRaw("Vertical");
+		transform.position = new Vector2(h, v);
+	}
+}
