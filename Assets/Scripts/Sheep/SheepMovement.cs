08287dd29ace40af6063e7baa1f5f6dab3da8256
Amir Eldor <amir@eize.ninja>
2016-12-18 00:56:30 +0200

    Sheep - very buggy initial movement

---
+++ Assets/Scripts/Sheep/SheepMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepMovement : MonoBehaviour {
+
+	new Rigidbody2D rigidbody;
+	BoxCollider2D boxCollider;
+
+	void Awake() {
+		rigidbody = GetComponent<Rigidbody2D>();
+		boxCollider = GetComponent<BoxCollider2D>();
+	}
+
+	void FixedUpdate() {
+		rigidbody.AddForceAtPosition(new Vector2(-5f, 0), new Vector2(boxCollider.bounds.size.x / 2f, 0));
+	}
+}
