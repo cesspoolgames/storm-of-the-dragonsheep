2736c93498e64aad6e3b57120c6f80501af0e9bd
Amir Eldor <Amir Eldor>
2016-12-17 17:08:20 +0200

    Attack animation + trigger

--- Assets/Scripts/Hero/HeroMovement.cs
+++ Assets/Scripts/Hero/HeroMovement.cs
@@ -7,9 +7,15 @@ public class HeroMovement : MonoBehaviour {
 	public float speed = 1f;
 
 	new Rigidbody2D rigidbody;
+	Animator animator;
 
 	void Awake() {
 		rigidbody = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
+	}
+
+	void Update() {
+		HandleAttack();
 	}
 
 	void FixedUpdate() {
@@ -21,5 +27,12 @@ public class HeroMovement : MonoBehaviour {
 		var velocity = rigidbody.velocity;
 		velocity.x = h * speed;
 		rigidbody.velocity = velocity;
+	}
+
+	void HandleAttack() {
+		var attackPressed = Input.GetButtonDown("Fire1");
+		if (attackPressed) {
+            animator.SetTrigger("Attack");
+        }
 	}
 }
