ea61d6d961e24db618085a68926fd4d9d03e1312
Amir Eldor <amire@eize.ninja>
2016-12-19 00:09:12 +0200

    Sheep prefab

--- Assets/Scripts/Sheep/SheepMovement.cs
+++ Assets/Scripts/Sheep/SheepMovement.cs
@@ -7,14 +7,22 @@ public class SheepMovement : MonoBehaviour {
 	public float moveForce = 40f;
 
 	new Rigidbody2D rigidbody;
-	BoxCollider2D boxCollider;
+	SpriteRenderer spriteRenderer;
 
 	void Awake() {
 		rigidbody = GetComponent<Rigidbody2D>();
-		boxCollider = GetComponent<BoxCollider2D>();
+		spriteRenderer = GetComponent<SpriteRenderer>();
 	}
 
 	void FixedUpdate() {
-		rigidbody.AddForce(new Vector2(-moveForce, 0));
+		var force = new Vector2(moveForce, 0);
+		if (isGoingLeft()) {
+			force *= -1;
+		}
+		rigidbody.AddForce(force);
+	}
+
+	public bool isGoingLeft() {
+		return spriteRenderer.flipX == true;
 	}
 }
