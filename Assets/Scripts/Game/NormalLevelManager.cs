72e60efbbd427eeac3c298ad589f560bdaba0b58
Amir Eldor <amire@eize.ninja>
2016-12-23 23:24:23 +0200

    Add MEC. Stupid sheep creation, need to fix ground

---
+++ Assets/Scripts/Game/NormalLevelManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MovementEffects;
+
+public class NormalLevelManager : MonoBehaviour {
+
+	public GameObject sheep;
+	public float sheepCreationDelay = 3f;
+
+	// Use this for initialization
+	void Start () {
+		Timing.RunCoroutine(_SheepCreationLoop(), "spawn");
+	}
+
+	IEnumerator<float> _SheepCreationLoop() {
+		while (true) {
+			yield return Timing.WaitForSeconds(sheepCreationDelay);
+			var newSheep = Instantiate(sheep);
+		}
+	}
+}
