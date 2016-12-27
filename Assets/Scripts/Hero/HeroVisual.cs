ad8403a47ac8928c7b43ddb59b2adf54d17c0d08
Amir Eldor <amir@eize.ninja>
2016-12-27 23:30:07 +0200

    HitArea on Hero destroys sheep (always)

--- Assets/Scripts/Hero/HeroVisual.cs
+++ Assets/Scripts/Hero/HeroVisual.cs
@@ -3,11 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HeroVisual : MonoBehaviour {
-	SpriteRenderer spriteRenderer;
-
-	void Awake() {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-	}
 
 	void Update() {
 		var horizontal = Input.GetAxisRaw("Horizontal");
---
+++ Assets/Scripts/Sheep/SheepAction.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepAction : MonoBehaviour {
+	public void Die() {
+		GetComponent<SheepMovement>().gameObject.SetActive(false);
+	}
+}
