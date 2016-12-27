d0f972affe64450fcf4637a597fc81161d4b0f57
Amir Eldor <amir@eize.ninja>
2016-12-27 23:36:33 +0200

    Sheep is hit only when attacking!

---
+++ Assets/Scripts/Hero/HeroAction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroAction : MonoBehaviour {
+
+	HeroHitArea hitArea;
+
+	void Awake() {
+		hitArea = GetComponentInChildren<HeroHitArea>();
+		hitArea.gameObject.SetActive(false);
+	}
+
+	public void HitStart() {
+		Debug.Log("Hit start");
+		hitArea.gameObject.SetActive(true);
+	}
+
+	public void HitEnd() {
+		Debug.Log("Hit end");
+		hitArea.gameObject.SetActive(false);
+	}
+}
