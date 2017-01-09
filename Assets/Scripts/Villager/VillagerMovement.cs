139a947af248a2533da38a55de0e371439010f8b
Amir Eldor <amir@eize.ninja>
2017-01-09 18:55:49 +0200

    Villager running to the right, not colliding with hero

---
+++ Assets/Scripts/Villager/VillagerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VillagerMovement : MonoBehaviour {
+
+	public float speed = 1f;
+
+	new Rigidbody2D rigidbody;
+
+	void Awake() {
+		rigidbody = GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate() {
+		Vector2 tempVelocity = rigidbody.velocity;
+		tempVelocity.x = speed;
+		rigidbody.velocity = tempVelocity;
+	}
+
+}
