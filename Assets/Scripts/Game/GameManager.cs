ca37b199e10cae328578ff96de1ba8cf445e310c
Amir Eldor <amir@eize.ninja>
2017-01-21 21:05:45 +0200

    Reformat GameManager.cs (only file that needed formatting???)

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -4,51 +4,51 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-	private static GameManager instance;
+    private static GameManager instance;
 
-	public static GameManager Instance {
-		get {
-			return instance;
-		}
-	}
+    public static GameManager Instance {
+        get {
+            return instance;
+        }
+    }
 
-	public enum EnemyStartPosition {
-		Left,
-		Right
-	};
+    public enum EnemyStartPosition {
+        Left,
+        Right
+    };
 
-	private static float levelLeft;
-	private static float levelRight;
+    private static float levelLeft;
+    private static float levelRight;
 
-	void Awake() {
-		if (instance != null && instance != this) {
-			Destroy(gameObject);
-		}
-		instance = this;
+    void Awake() {
+        if (instance != null && instance != this) {
+            Destroy(gameObject);
+        }
+        instance = this;
 
-		levelLeft = Camera.main.ViewportToWorldPoint(new Vector2(0f, 0f)).x;
-		levelRight = Camera.main.ViewportToWorldPoint(new Vector2(1f, 0f)).x;
-	}
+        levelLeft = Camera.main.ViewportToWorldPoint(new Vector2(0f, 0f)).x;
+        levelRight = Camera.main.ViewportToWorldPoint(new Vector2(1f, 0f)).x;
+    }
 
-	public static float LevelLeft {
-		get {
+    public static float LevelLeft {
+        get {
             return levelLeft;
-		}
-	}
+        }
+    }
 
-	public static float LevelRight {
-		get {
+    public static float LevelRight {
+        get {
             return levelRight;
-		}
-	}
+        }
+    }
 
 
-	public static EnemyStartPosition RandomEnemyStartPosition() {
-		if (Random.value > 0.5f) {
-			return EnemyStartPosition.Left;
-		} else {
-			return EnemyStartPosition.Right;
-		}
-	}
+    public static EnemyStartPosition RandomEnemyStartPosition() {
+        if (Random.value > 0.5f) {
+            return EnemyStartPosition.Left;
+        } else {
+            return EnemyStartPosition.Right;
+        }
+    }
 
 }
