c5aef3e7851bbc8b07e3e4bab7804895d2466716
Amir Eldor <amir@eize.ninja>
2017-01-25 22:21:42 +0200

    Disable villager animation on death

--- Assets/Scripts/Villager/VillagerVisual.cs
+++ Assets/Scripts/Villager/VillagerVisual.cs
@@ -13,6 +13,7 @@ public class VillagerVisual : MonoBehaviour {
     void Update() {
         if (movementScript.IsFallen()) {
             transform.rotation = Quaternion.Euler(0, 0, -90f);
+            GetComponent<Animator>().enabled = false;
         } else {
             transform.rotation = Quaternion.Euler(0, 0, 0);
         }
