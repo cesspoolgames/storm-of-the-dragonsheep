0a218dceb6c64ba46cd6f912525fda3db4a054dc
Amir Eldor <amir@eize.ninja>
2017-02-02 00:17:16 +0200

    Sheep Y start offset randomization

--- Assets/Scripts/Game/LevelSheepGenerator.cs
+++ Assets/Scripts/Game/LevelSheepGenerator.cs
@@ -5,40 +5,41 @@ using MovementEffects;
 
 public class LevelSheepGenerator : MonoBehaviour {
 
-	public float sheepGenerationDelay = 1.2f;
-	public GameObject sheep;
-	public float sheepOffscreenOffset = 1f;
+    public float sheepGenerationDelay = 1.2f;
+    public GameObject sheep;
+    public float sheepOffscreenOffset = 1f;
+    public float sheepMaxStartYOffset = 2f;
 
-	// Use this for initialization
-	void Start () {
-		Timing.CallDelayed<IEnumerator<float>>(null, sheepGenerationDelay, _ => {
-			Timing.RunCoroutine(_SheepGenerationLoop());
-		});
-	}
-	
-	// Update is called once per frame
-	IEnumerator<float> _SheepGenerationLoop() {
-		while (true) {
-			CreateRandomSheep();
-			yield return Timing.WaitForSeconds(sheepGenerationDelay);
-		}
-	}
+    // Use this for initialization
+    void Start() {
+        Timing.CallDelayed<IEnumerator<float>>(null, sheepGenerationDelay, _ => {
+            Timing.RunCoroutine(_SheepGenerationLoop());
+        });
+    }
 
-	void CreateRandomSheep() {
-		GameManager.EnemyStartPosition sheepStartPosition = GameManager.RandomEnemyStartPosition();
-		Vector2 newPosition;
+    // Update is called once per frame
+    IEnumerator<float> _SheepGenerationLoop() {
+        while (true) {
+            CreateRandomSheep();
+            yield return Timing.WaitForSeconds(sheepGenerationDelay);
+        }
+    }
 
-		newPosition.y = 0f;
-		bool flipX = false;
-		if (sheepStartPosition == GameManager.EnemyStartPosition.Left) {
-			newPosition.x = GameManager.LevelLeft - sheepOffscreenOffset;
-		} else {
-			newPosition.x = GameManager.LevelRight + sheepOffscreenOffset;
-			flipX = true;
-		}
+    void CreateRandomSheep() {
+        GameManager.EnemyStartPosition sheepStartPosition = GameManager.RandomEnemyStartPosition();
+        Vector2 newPosition;
 
-		var newSheep = Instantiate(sheep, newPosition, Quaternion.identity) as GameObject;
-		newSheep.GetComponent<SpriteRenderer>().flipX = flipX;
-	}
+        newPosition.y = Random.value * sheepMaxStartYOffset;
+        bool flipX = false;
+        if (sheepStartPosition == GameManager.EnemyStartPosition.Left) {
+            newPosition.x = GameManager.LevelLeft - sheepOffscreenOffset;
+        } else {
+            newPosition.x = GameManager.LevelRight + sheepOffscreenOffset;
+            flipX = true;
+        }
+
+        var newSheep = Instantiate(sheep, newPosition, Quaternion.identity) as GameObject;
+        newSheep.GetComponent<SpriteRenderer>().flipX = flipX;
+    }
 
 }
